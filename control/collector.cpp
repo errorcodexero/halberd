9425a27aa79530c3a90b89e41c073cfde22d9120
Software B <SoftwareB>
2015-12-10 20:46:22 -0800

    Made progress on collector code (doesn't pass tests)

--- control/collector.cpp
+++ control/collector.cpp
@@ -4,17 +4,39 @@
 using namespace std;
 
 ostream& operator<<(ostream& o, Collector::Goal a){
-	if(a==Collector::Goal::OFF)return o<<"Goal(off)";
-	if(a==Collector::Goal::REVERSE)return o<<"Goal(reverse)";
-	if(a==Collector::Goal::FORWARD)return o<<"Goal(forward)";
+	if(a==Collector::Goal::OFF) return o<<"Goal(off)";
+	if(a==Collector::Goal::REVERSE) return o<<"Goal(reverse)";
+	if(a==Collector::Goal::FORWARD) return o<<"Goal(forward)";
 	assert(0);
 }
 
 ostream& operator<<(ostream& o, Collector a){
-	return o<<"Collector(goal:"<<a.goal<<" status:"<<a.status<<" output:"<<a.output<<")";
+	return o<<"Collector("<<")";
 }
 
-#ifdef KICKER_TEST
+ostream& operator<<(ostream& o, Collector::Status_detail){ return o<<" ";}
+ostream& operator<<(ostream& o, Collector::Input){ return o<<" ";}
+
+bool operator==(Collector::Input,Collector::Input){ return true;}
+bool operator<(Collector::Status_detail, Collector::Status_detail){ return 1;}
+bool operator==(Collector::Status_detail, Collector::Status_detail){ return 1;}
+
+Collector::Output control(Collector::Status_detail, Collector::Goal goal){
+	return goal;
+}
+
+Collector::Status status(Collector::Status_detail){return {};}
+
+bool ready(Collector::Status, Collector::Goal){return 1;}
+
+set<Collector::Goal> examples(Collector::Goal*){ 
+	return {Collector::Goal::FORWARD,Collector::Goal::OFF,Collector::Goal::REVERSE};
+}
+set<Collector::Input> examples(Collector::Input*){ return{};}
+set<Collector::Input_reader> examples(Collector::Input_reader){ return {};}
+set<Collector::Status> examples(Collector::Status*){ return {};}
+
+#ifdef COLLECTOR_TEST
 #include "formal.h"
 
 int main(){
--- control/collector.h
+++ control/collector.h
@@ -3,20 +3,46 @@
 
 #include <iostream>
 #include "formal.h"
+#include <set>
 
 using namespace std;
 
 struct Collector{
 	enum class Goal{FORWARD,OFF,REVERSE};
-	Goal goal;
-	typedef Goal Status_detail;
-	Status_detail status; 
-	typedef Goal Output;	
+	Goal goal; 
+	
+	struct Status_detail{};
+	Status_detail status_detail;
+
+	typedef Status_detail Status;
+	Status status;
+
+	struct Input{};
+	Input input;
+	
+	struct Input_reader{
+		Input operator()(Robot_inputs)const{ return Input{};}
+		Robot_inputs operator()(Robot_inputs a,Input)const{ return a;}
+	};
+	Input_reader input_reader;
+
+	typedef Goal Output;
 	Output output;
 	
-	Collector():goal(Goal::OFF),status(Status_detail::OFF),output(Output::OFF){}
-};
+	struct Output_applicator{
+		Robot_outputs operator()(Robot_outputs a,Output)const{ return a; }
+		Output operator()(Robot_outputs)const{ return Output{}; }	
+	};
+	Output_applicator output_applicator;
 
+	struct Estimator{
+		Status_detail get()const{ return Status_detail{};}
+		void update(Time,Input,Output){};	
+	};
+	Estimator estimator;
+	
+	Collector():goal(Goal::OFF){}
+};
 
 ostream& operator<<(ostream& o, Collector::Goal a);
 ostream& operator<<(ostream& o, Collector a);
